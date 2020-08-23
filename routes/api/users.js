13164cacc5ccc77242ae7030a8441e191f3ca935
austinwilson1224 <austin1224@gmail.com>
2020-08-22 21:58:20 -0700

    more node stuff

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 
-// @route  GET api/users
-// @desc   test route
+// @route  POST api/users
+// @desc   register user
 // @access Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+    '/',
+    [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
+    ], 
+    (req, res) => {
+        // console.log(req.body);
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array()});
+        }
+
+        // see if the user exists
+        res.send('user route');
+    }   
+);
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const connectDB = require('./config/db');
 app = express();
 // connect database
 connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false}));
 app.get('/', (req,res) => res.send("API running"));
 
 // Define routes
