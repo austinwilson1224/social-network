5434708c2fe3c46cdf8f3026679f9d8e015c1ea1
austinwilson1224 <austin1224@gmail.com>
2020-08-25 15:30:23 -0700

    profile stuff

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require("express-validator/check");
 
 
 const Profile = require('../../models/Profile');
@@ -22,5 +23,23 @@ router.get('/', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+// @route  POST api/profile
+// @desc   Create or update user profile
+// @access Private
+router.post(
+    '/', 
+    [ auth, [
+        check('status', 'Status is required').not().isEmpty(),
+        check('skills', 'Skills is required').not().isEmpty()
+        ]
+    ],
+    async (req,res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+})
 
 module.exports = router;
